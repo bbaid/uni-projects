99cf19a84d67b07f60733dd1c304c30de2d209f0
bbaid <31852450+bbaid@users.noreply.github.com>
2025-01-22 00:05:41 +0200

    Added intital version of Twenty-minute-timer

---
+++ Coding/Twenty-rule-timer/twenty-rule-timer.py
@@ -0,0 +1,15 @@
+# A timer which promts the user to take a break every 20 minutes. After 20 seconds the timer will start again.
+
+import time
+
+while True:
+    time.sleep(20*60)  # 20 minutes
+    print("Time to take a break!")
+    time.sleep(20)
+    print("Time to get back to work!")
+
+# This script will run indefinitely until the user stops it.
+
+# To stop the script, press Ctrl + C in the terminal. This will send a KeyboardInterrupt signal to the script, which will cause it to stop running.
+# If you are using an IDE, you may need to press a different key combination to stop the script. Check the documentation for your IDE to find out how to stop a running script.
+# If you are running the script in a terminal window, you can also close the terminal window to stop the script.
